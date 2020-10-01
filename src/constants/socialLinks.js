88d1b33f59dbb090d226faa578470c0408a7df96
AppleConnoiseur <chjeesy@gmail.com>
2020-10-01 07:52:40 +0200

    Initial commit.

---
+++ src/constants/socialLinks.js
@@ -0,0 +1,46 @@
+import React from "react"
+import {
+  FaFacebookSquare,
+  FaLinkedin,
+  FaDribbbleSquare,
+  FaBehanceSquare,
+  FaTwitterSquare,
+} from "react-icons/fa"
+
+const data = [
+  {
+    id: 1,
+    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
+    url: "https://www.facebook.com/chjees/",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    url: "https://www.linkedin.com/in/jesper-eriksson-a55060173/",
+  },
+  {
+    id: 3,
+    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
+    url: "#",
+  },
+  {
+    id: 4,
+    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
+    url: "#",
+  },
+]
+const links = data.map(link => {
+  return (
+    <li key={link.id}>
+      <a href={link.url} className="social-link">
+        {link.icon}
+      </a>
+    </li>
+  )
+})
+
+export default ({ styleClass }) => {
+  return (
+    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import Blogs from "../components/Blogs"
+
+var MyName = 'Jesper E';
+const NameComponent = ({ fancy }) => {
+	if (fancy) {
+		return (<h3>{MyName}</h3>);
+	}
+	return (<p>{MyName}</p>);
+};
+
+export default () => {
+	return (
+		<Layout>
+			<Hero />
+			<Services />
+		</Layout>
+	)
+}
+// ...GatsbyImageSharpFluid
