6243908a380223ce496f62deb6831e6ce5f74d99
AppleConnoiseur <chjeesy@gmail.com>
2020-10-07 16:27:44 +0200

    Added about.

--- portfolio-frontend/gatsby-config.js
+++ portfolio-frontend/gatsby-config.js
@@ -6,10 +6,10 @@
 
 module.exports = {
   siteMetadata: {
-    title: "WebDev Portfolio",
-    description: "This is WebDev Portfolio Site",
-    author: "@webdev",
-    twitterUsername: "@john_smilga",
+    title: "Jesper's Portfolio",
+    description: "This is Jesper Eriksson's developer portfolio.",
+    author: "Jesper Eriksson",
+    //twitterUsername: "",
     image: "/twitter-img.png",
     siteUrl: "https://testing-strapi-gatsby-build.netlify.app",
   },
@@ -30,10 +30,8 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        //   singleType : `about`
-        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
         contentTypes: [`jobs`,`projects`,`blogs`],
-        singleTypes: [],
+        singleTypes: [`about`],
       },
     },
     // {
--- portfolio-frontend/src/pages/about.js
+++ portfolio-frontend/src/pages/about.js
@@ -2,10 +2,71 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
+import ReactMarkdown from "react-markdown"
 import Image from "gatsby-image"
+import { Link } from "gatsby"
+import downloadFile from "../../static/CV-JesperE.pdf"
 // ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+const About = ({
+	data: {
+		about: { nodes },
+	},
+}) => {
+	//console.log(nodes)
+	const { info, stack, title, image } = nodes[0]
+	return (
+		<Layout>
+			<section className="about-page">
+				<div className="section-center about-center">
+					<Image fluid={image.childImageSharp.fluid} className="about-img" />
+					<article className="about-text">
+						{/* <Title title={title} />
+				  <p>{info}</p> */}
+						<ReactMarkdown source={info} />
+						<div className="about-stack">
+							{stack.map(item => {
+								return <span key={item.id}>{item.title}</span>
+							})}
+						</div>
+
+						<div className="btn cv">
+							<a href={downloadFile} download>
+								Ladda ner fullständig CV
+				</a>
+							{` `}
+						</div>
+						<Link to="/contact" className="btn">
+							kontakta mig
+			  </Link>
+					</article>
+				</div>
+				<></>
+			</section>
+		</Layout>
+	)
 }
+
+export const query = graphql`
+	{
+	  about: allStrapiAbout {
+		nodes {
+		  stack {
+			id
+			title
+		  }
+		  title
+		  info
+		  image {
+			childImageSharp {
+			  fluid {
+				...GatsbyImageSharpFluid
+			  }
+			}
+		  }
+		}
+	  }
+	}
+  `
 
 export default About
